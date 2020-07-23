d42f2c4a4da8d9ce646e7a27d1d86bb304191b87
Jaime Escobar <jaime.escobar.mtz@gmail.com>
2020-07-23 16:29:28 -0700

    Update Contents.swift

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,16 +1,16 @@
-import Foundation
+import Cocoa
+import CreateML
 
-func fibonacci(n: Int) {
-    var res: [Int] = []
-    for number in 0..<n {
-        if number < 2{
-            res.append(number)
-        } else {
-            let sum = res[number-2] + res[number-1]
-            res.append(sum)
-        }
-    }
-    print(res)
-}
+let data = try MLDataTable(contentsOf: URL(fileURLWithPath: "/Users/jaime/Downloads/twitter-sanders-apple3.csv"))
+let (trainingData, testingData) = data.randomSplit(by: 0.8, seed: 5)
 
-fibonacci(n: 10)
+let classifier = try MLTextClassifier(trainingData: trainingData, textColumn: "text", labelColumn: "class")
+let evaluation = classifier.evaluation(on: testingData, textColumn: "text", labelColumn: "class")
+let accuracy = (1.0 - evaluation.classificationError) * 100
+
+let metadata = MLModelMetadata(author: "Jaime Escobar", shortDescription: "A tweets text emotion recognition", version: "1.0")
+try classifier.write(to: URL(fileURLWithPath: "/Users/jaime/Downloads/TweetsClassifier.mlmodel"))
+
+try classifier.prediction(from: "@Apple is terrible")
+try classifier.prediction(from: "I'm Mexican")
+try classifier.prediction(from: "I love to work at @IBM")
